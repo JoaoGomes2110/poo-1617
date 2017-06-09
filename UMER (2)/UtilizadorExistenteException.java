7b2a633e395220ee5a5478de5a93e2cbd9e9270e
Joao Gomes <a74033@alunos.uminho.pt>
2017-06-09 13:55:25 +0100

    Eliminada versão anterior

--- UMER (2)/UtilizadorExistenteException.java
+++
@@ -1,9 +0,0 @@
-public class UtilizadorExistenteException extends Exception implements java.io.Serializable {
-    
-    public UtilizadorExistenteException() {
-        
-        super("\nO utilizador ja existe!");
-        
-    }
-    
-}
